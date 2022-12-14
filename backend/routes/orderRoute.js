ceb22d6e76f7b47078ed8f4ecd1e87ff96f30719
aakash <asoni1940@gmail.com>
2022-12-15 00:31:47 +0530

    order controller api

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,9 +1,20 @@
 const express=require("express")
-const { newOrder } = require("../controller/orderController")
+const { newOrder, getSingleOrder, myOrders, getAllOrders, updateOrder, deleteOrder } = require("../controller/orderController")
 
 const router=express.Router()
 
-router.route("/order/new").post(newOrder)
+const { isAuthenticatedUser , authorizeRoles} = require("../middleware/auth");
 
 
-module.exports=router
+router.route("/order/new").post( isAuthenticatedUser,newOrder)
+router.route("/order/:id").get( isAuthenticatedUser,getSingleOrder)
+router.route("/orders/me").get( isAuthenticatedUser,myOrders)
+
+
+
+router.route("/admin/orders").get(isAuthenticatedUser,authorizeRoles("admin"),getAllOrders)
+router.route("/admin/order/:id").put(isAuthenticatedUser,authorizeRoles("admin"),updateOrder)
+router.route("/admin/order/:id").delete(isAuthenticatedUser,authorizeRoles("admin"),deleteOrder)
+
+
+module.exports=router  
